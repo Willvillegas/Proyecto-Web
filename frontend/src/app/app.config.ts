964e337c32e21e7ab4b7a2399c3c59176d2679e7
Willvilegas <eduardo78villegas@estudiantec.cr>
2025-12-01 15:31:28 -0600

    Update Angular dependencies and refactor app configuration for zoneless change detection

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
-  provideAnimationsAsync(),
-  provideHttpClient(withFetch()), provideAnimationsAsync()
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
   ]
 };
 
